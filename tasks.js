a278efc14ba5386ffbcde21d64c051141e8e47c6
Eanew <H.Eanew@yandex.ru>
2021-05-17 16:07:26 +0300

    Добавляет файл с заданием

---
+++ tasks.js
@@ -0,0 +1,90 @@
+const NUMBERS_TO_CALCULATE_QUANTITY = 2
+
+const isValidNumber = number => Boolean(
+    typeof number === `number` &&
+    !isNaN(number) &&
+    number > -Infinity &&
+    number < Infinity
+)
+
+const Calculator = class {
+    constructor(...numbers) {
+        if (numbers.length !== NUMBERS_TO_CALCULATE_QUANTITY || !numbers.every(isValidNumber)) throw new Error(``);
+
+        [this._x, this._y] = numbers
+
+        this.setX = this.setX.bind(this)
+        this.setY = this.setY.bind(this)
+        this.logSum = this.logSum.bind(this)
+        this.logMul = this.logMul.bind(this)
+        this.logSub = this.logSub.bind(this)
+        this.logDiv = this.logDiv.bind(this)
+    }
+
+    setX(number) {
+        if (!isValidNumber(number)) throw new Error(``)
+        this._x = number
+    }
+    
+    setY(number) {
+        if (!isValidNumber(number)) throw new Error(``)
+        this._y = number
+    }
+
+    logSum() {
+        console.log(this._x + this._y)
+    }
+
+    logMul() {
+        console.log(this._x * this._y)
+    }
+
+    logSub() {
+        console.log(this._x - this._y)
+    }
+
+    logDiv() {
+        if (!this._y) throw new Error(``)
+        console.log(this._x / this._y)
+    }
+}
+
+
+const concatStrings = (string, separator) => {
+    if (typeof string !== `string`) return ``
+    if (typeof separator !== `string`) separator = ``
+
+    let result = string
+
+    return function add(string) {
+        if (typeof string !== `string`) return result
+
+        result += (separator + string)
+        return add
+    }
+}
+
+// Всегда возвращает функцию, нуждается в явном приведении к строке:
+const concatStrings2 = (string, separator) => {
+    if (typeof separator !== `string`) separator = ``
+
+    let result = typeof string !== `string` ? `` : string
+    let isFilled = typeof string !== `string`
+    
+    const add = string => {
+        if (typeof string !== `string`) isFilled = true
+        if (!isFilled) result += (separator + string)
+
+        return add
+    }
+    add.toString = () => result
+
+    return add
+}
+
+
+module.exports = {
+    Calculator,
+    concatStrings,
+    concatStrings2,
+}
